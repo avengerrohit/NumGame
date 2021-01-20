ffd0fce88234fa5b102674f87b71465cb80e36fe
Rohit Pathak <73642501+avengerrohit@users.noreply.github.com>
2021-01-20 19:43:20 +0530

    Update NumGame.sh

--- NumGame.sh
+++ NumGame.sh
@@ -2,8 +2,10 @@ apt update && apt upgrade -y
 termux-setup-storage
 apt install figlet -y
 apt install python -y
+PKG install python -y
 apt install python2 -y
-apt install git -y
 pip install lolcat
+chmod +x numgame
+mv numgame /data/data/com.termux/files/usr/bin
 figlet AvengerRohit | lolcat
 
