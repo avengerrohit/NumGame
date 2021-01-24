08931c735596cd8745368b9db31c811d73d37e4b
Rohit Pathak <73642501+avengerrohit@users.noreply.github.com>
2021-01-24 18:11:39 +0530

    Update numgame.py

--- numgame.py
+++ numgame.py
@@ -1,9 +1,9 @@
 counter = 1
-n = 10000000
+n = 600000
 
 while counter < n+1:
 
-    if counter % 6 == 0:
+    if counter % 10 == 0:
         print(counter)
     else:
         print("\033[1;32;40m", counter, end=" ") #Change this line to print counter, for python version 2
